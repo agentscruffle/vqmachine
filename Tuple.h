851effbeb649708cd3fff4495a0cb054e95b222a
eddiemrogers@gmail.com <eddiemrogers@gmail.com@e894adcf-cd6d-6d68-49a7-318451db4687>
2014-02-25 14:32:04 +0000

    Initial Version

---
+++ Tuple.h
@@ -0,0 +1,22 @@
+/*
+ * Tuple.h
+ *
+ *  Created on: Feb 13, 2014
+ *      Author: VQMTEAM
+ */
+
+#ifndef TUPLE_H_
+#define TUPLE_H_
+
+namespace Machine {
+
+struct Tuple {
+public:
+
+	double x;
+	double y;
+
+};
+
+} /* namespace Machine */
+#endif /* TUPLE_H_ */
