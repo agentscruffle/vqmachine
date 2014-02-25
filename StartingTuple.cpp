9c69479f7aa84c5a20fcc29c124806aa7895a876
eddiemrogers@gmail.com <eddiemrogers@gmail.com@e894adcf-cd6d-6d68-49a7-318451db4687>
2014-02-25 14:31:00 +0000

    Initial Version

---
+++ StartingTuple.cpp
@@ -0,0 +1,21 @@
+/*
+ * StartingTuple.cpp
+ *
+ *  Created on: Feb 13, 2014
+ *      Author: eddie
+ */
+
+#include "StartingTuple.h"
+
+namespace Machine {
+
+StartingTuple::StartingTuple() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StartingTuple::~StartingTuple() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Machine */
