147b8bf6f682ce4bcb3b82b016eef9ddfb37e3f4
eddiemrogers@gmail.com <eddiemrogers@gmail.com@e894adcf-cd6d-6d68-49a7-318451db4687>
2014-02-25 14:28:44 +0000

    Initial Version

---
+++ EndingTuple.cpp
@@ -0,0 +1,21 @@
+/*
+ * EndingTuple.cpp
+ *
+ *  Created on: Feb 13, 2014
+ *      Author: VQMTEAM
+ */
+
+#include "EndingTuple.h"
+
+namespace Machine {
+
+EndingTuple::EndingTuple() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EndingTuple::~EndingTuple() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Machine */
