ab91a3087bb97129bd0493e6c58d308b9411bd28
eddiemrogers@gmail.com <eddiemrogers@gmail.com@e894adcf-cd6d-6d68-49a7-318451db4687>
2014-02-25 14:29:02 +0000

    Initial Version

---
+++ EndingTuple.h
@@ -0,0 +1,22 @@
+/*
+ * EndingTuple.h
+ *
+ *  Created on: Feb 13, 2014
+ *      Author: VQMTEAM
+ */
+
+#ifndef ENDINGTUPLE_H_
+#define ENDINGTUPLE_H_
+
+#include "Tuple.h"
+
+namespace Machine {
+
+class EndingTuple : Tuple {
+public:
+	EndingTuple();
+	virtual ~EndingTuple();
+};
+
+} /* namespace Machine */
+#endif /* ENDINGTUPLE_H_ */
